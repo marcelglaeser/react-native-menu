f0bcd1350513670b54496687878b129191a187be
Marcel Gläser <marcelglaeser@users.noreply.github.com>
2020-11-18 10:57:33 +0100

    change componentWillMount to componentDidMount

--- src/menu/makeMenu.js
+++ src/menu/makeMenu.js
@@ -34,7 +34,7 @@ module.exports = (React, ReactNative, { constants, model, styles }) => {
     getChildContext() {
       return { getClosestMenuName: () => this._name };
     },
-    componentWillMount() {
+    componentDidMount() {
       this._name = this.props.name || this.context.menuController.makeName();
     },
     componentWillUnmount() {
